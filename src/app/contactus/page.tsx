ebd50a4ce4cea34676bdff162d11127286ec1378
ShowyCone <alexrafael2003@gmail.com>
2025-07-25 15:29:07 -0400

    Add Contact Us, Login, and Sign Up pages with responsive design and form handling

---
+++ src/app/contactus/page.tsx
@@ -0,0 +1,6 @@
+'use client'
+import ContactUs from '@/components/ContactUs'
+
+export default function ContactUsPage() {
+  return <ContactUs />
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,6 @@
+'use client'
+import SignUp from '@/components/SignUp'
+
+export default function SignUpPage() {
+  return <SignUp />
+}
